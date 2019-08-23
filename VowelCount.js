6ba08f64bb90e8c00c1a355e4d62838fc1b8f641
RahniKaurBansal <48180359+RahniKaurBansal@users.noreply.github.com>
2019-08-23 20:13:50 +0200

    Update VowelCount.js

--- VowelCount.js
+++ VowelCount.js
@@ -1,7 +1,7 @@
-const findVowels2 = str => {
+const findVowels = str => {
 const matched = str.match(/[aeiou]/gi)
 console.log(matched ? matched.length : 0) 
 }
-findVowels2('vowels')
+findVowels('vowels')
 
 
