35bce816e4c150a309e3cca6c625574122722a07
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-06-27 21:22:49 +0000

    learnpack clean ran

--- .learn/resets/04-VowelCount/app.js
+++
@@ -1,8 +0,0 @@
-// your code here
-function findVowels(word){
- const matched = word.match(/[aeiou]/gi);
- console.log(matched ? matched.length : 0); 
- return matched ? matched.length : 0;
-}
-
-findVowels("carolas")
